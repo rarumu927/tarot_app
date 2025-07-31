3fa97e034598940355f03e5a94c36ad23fae0763
rarum <break927@gmail.com>
2025-07-31 18:09:07 +0900

    Fix in TarotController

--- src/main/java/com/example/TarotController.java
+++ src/main/java/com/example/TarotController.java
@@ -3,24 +3,18 @@ package com.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// public class TarotController {
-
-//     private final TarotService tarotService;
-
-//     public TarotController(TarotService tarotService) {
-//         this.tarotService = tarotService;
-//     }
-
-//     @GetMapping("/tarot")
-//     public TarotResultDto getTarotReading() {
-//         return tarotService.drawCard();
-//     }
-// }
 @RestController
 public class TarotController {
-    @GetMapping("/")
-    public String home() {
-        return "Hello from Tarot app!";
+
+    private final TarotService tarotService;
+
+    public TarotController(TarotService tarotService) {
+        this.tarotService = tarotService;
     }
+
+    @GetMapping("/tarot")
+    public TarotResultDto getTarotReading() {
+        return tarotService.drawCard();
+    }
+
 }
