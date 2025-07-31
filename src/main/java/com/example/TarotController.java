cf86546ae19008c40d0aaef6f0a2ae3f3db6fcfa
rarum <break927@gmail.com>
2025-07-31 17:53:51 +0900

    Add HomeController

--- src/main/java/com/example/TarotController.java
+++ src/main/java/com/example/TarotController.java
@@ -3,18 +3,24 @@ package com.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// @RestController
+// public class TarotController {
+
+//     private final TarotService tarotService;
+
+//     public TarotController(TarotService tarotService) {
+//         this.tarotService = tarotService;
+//     }
+
+//     @GetMapping("/tarot")
+//     public TarotResultDto getTarotReading() {
+//         return tarotService.drawCard();
+//     }
+// }
 @RestController
 public class TarotController {
-
-    private final TarotService tarotService;
-
-    public TarotController(TarotService tarotService) {
-        this.tarotService = tarotService;
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Tarot app!";
     }
-
-    @GetMapping("/tarot")
-    public TarotResultDto getTarotReading() {
-        return tarotService.drawCard();
-    }
-
 }
